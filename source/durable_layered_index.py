c16d5add48e78b88115e4952c968ebd37cc88f81
Uchechukwu Uche-Ike <uuchei2@illinois.edu>
2025-11-18 15:56:50 -0600

    Feat: Multi Bin DLI

--- source/durable_layered_index.py
+++ source/durable_layered_index.py
@@ -1,70 +1,50 @@
 import asyncio
+from bin import Bin
 from query import Query
+import numpy as np
+import faiss
+
 
 class DurableLayeredIndex:
-    def __init__(self, max_batch_size: int = 10,
-                 max_wait_secs: float = 0.5,
-                 grouping_threshold: float = 0.9):
-        self.max_batch_size = max_batch_size
-        self.max_wait_secs = max_wait_secs
-        self.grouping_threshold = grouping_threshold
+    def __init__(self, num_bins = 1, grouping_threshold: float = 0.9):
+        #Maps vectors to a bin
+        self._vector_bin_index = faiss.IndexFlatL2(384)
+        self._vector_bin_map = {}
+        self._add_bin()
 
-        self._queue = []
-        self._lock = asyncio.Lock()
-        self._task = asyncio.create_task(self._batch_loop())
+    def _add_bin(self):
+        #TODO: Using random vector for now, ideally this should be a param
+        bin_vector = np.random.randn(1, 384).astype('float32')
+        self._vector_bin_index.add(bin_vector)
+        bin_index = self._vector_bin_index.ntotal - 1
+        self._vector_bin_map[bin_index] = Bin()
 
     async def query(self, query_text: str):
         future = asyncio.get_running_loop().create_future()
-        q = Query(query_text, future)
-
-        async with self._lock:
-            self._queue.append(q)
-            if len(self._queue) >= self.max_batch_size:
-                # cancel the waiting task so we flush now
-                self._task.cancel()
-                # start a new task for next batch
-                self._task = asyncio.create_task(self._batch_loop())
-
+        new_query = Query(query_text, future)
+        await self._add_query_to_bin(new_query)
         return await future
+    
+    async def _add_query_to_bin(self, new_query):
+        emb = new_query.get_embedding().astype('float32').reshape(1, -1)
+        distances, indices = self._vector_bin_index.search(emb, k=1)
+        bin_index = indices[0][0]
+        selected_bin = self._vector_bin_map[bin_index]
+        await selected_bin.add_query(new_query)
 
-    async def _batch_loop(self):
-        try:
-            await asyncio.sleep(self.max_wait_secs)
-        except asyncio.CancelledError:
-            # we cancelled early because of batch_size
-            pass
-        await self._process_batch()
 
-    async def _process_batch(self):
-        async with self._lock:
-            batch = self._queue
-            self._queue = []
-        if not batch:
-            return
 
-        texts = [q.get_text() for q in batch]
-        embs = [q.get_embedding() for q in batch]
-        futures = [q.get_future() for q in batch]
+    async def close(self):
+        for bin in self._vector_bin_map.values():
+            await bin.shutdown()
 
-        # TODO: Add logic for reordering queries
-
-        results = await self._batch_vector_db_query(texts, embs)
-
-        for fut, res in zip(futures, results):
-            if not fut.done():
-                fut.set_result(res)
-
-    async def _batch_vector_db_query(self, texts, embs):
-        #TODO: Add code for accessing database here, just adding artificial wait
-        # for demo purposes
-        await asyncio.sleep(0.1)
-        return [f"Result for '{t}'" for t in texts]
 
 async def main():
-    dli = DurableLayeredIndex(max_batch_size=5, max_wait_secs=0.3)
+    dli = DurableLayeredIndex()
     queries = ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6"]
     tasks = [asyncio.create_task(dli.query(q)) for q in queries]
     results = await asyncio.gather(*tasks)
     print(results)
+    await dli.close()
 
 asyncio.run(main())
